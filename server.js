371e247c2b4210b340b1c93e031a0f355a9a5295
Claire Kwong <clairebkwong@gmail.com>
2025-11-12 15:39:07 -0500

    feat: turn servo

--- server.js
+++ server.js
@@ -7,14 +7,21 @@ const board = new five.Board();
 
 board.on("ready", () => {
   const led = new five.Led(13);
+  const servo = new five.Servo(10);
 
   wss.on("connection", (ws) => {
     ws.on("message", (message) => {
       const command = JSON.parse(message);
       if (command.action === "blink") {
+        console.log("blink LED");
         led.blink();
       } else if (command.action === "stop") {
+        console.log("stop LED");
         led.stop();
+      } else if (command.action === "turn") {
+        const angle = Math.floor(Math.random() * 360);
+        console.log("set servo to angle: " + angle);
+        servo.to(angle);
       }
     });
   });
